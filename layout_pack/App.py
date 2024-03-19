c5ac323c5bcd62588cb7b3342669d50aa5c573e0
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-03-19 08:32:41 -0300

    Creating more layout using pack

--- layout_pack/App.py
+++ layout_pack/App.py
@@ -6,17 +6,45 @@ window = tk.Tk()
 window.title('Layout Pack')
 window.geometry('400x600')
 
-#widgets
-label1 = ttk.Label(window, text='First Label', background='red')
-label2 = ttk.Label(window, text='Label 2', background='blue')
-label3 = ttk.Label(window, text='Last of the Labels', background='green')
-button = ttk.Button(window, text='Button')
+# top frame
+top_frame = ttk.Frame(window)
+label1 = ttk.Label(top_frame, text='First label', background='red')
+label2 = ttk.Label(top_frame, text='Label 2', background='blue')
 
-#layout
-label1.pack(side='top', expand=True, fill='both', padx=10, pady=10)
-label2.pack(side='left', expand=True, fill='both')
-label3.pack(side='top', expand=True, fill='both')
-button.pack(side='top', expand=True, fill='both') 
+# middle widget
+label3 = ttk.Label(window, text='Another label', background='green')
 
-#run
+# bottom frame
+bottom_frame = ttk.Frame(window)
+label4 = ttk.Label(bottom_frame, text='Last of the labels', background='orange')
+button = ttk.Button(bottom_frame, text= 'A Button')
+button2 = ttk.Button(bottom_frame, text= 'Another Button')
+
+# exercise widgets
+exercise_frame = ttk.Frame(bottom_frame)
+button3 = ttk.Button(exercise_frame, text= 'Button3')
+button4 = ttk.Button(exercise_frame, text= 'Button4')
+button5 = ttk.Button(exercise_frame, text= 'Button5')
+
+# top layout
+label1.pack(side='top', fill='both', expand=True)
+label2.pack(side='top', fill='both', expand=True)
+top_frame.pack(fill='both', expand=True)
+
+# middle layout
+label3.pack(expand=True)
+
+# bottom layout
+button.pack(side='left', expand=True, fill='both')
+label4.pack(side='left', expand=True, fill='both')
+button2.pack(side='left', expand=True, fill='both')
+bottom_frame.pack(expand=True, fill='both', padx=20, pady=20)
+
+# exercise layout
+button3.pack(expand=True, fill='both')
+button4.pack(expand=True, fill='both')
+button5.pack(expand=True, fill='both')
+exercise_frame.pack(side='left', expand=True, fill='both')
+
+# run
 window.mainloop()
